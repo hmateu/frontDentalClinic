fa301c3b59a1460ce2dff272717b7a68bc3dc4c5
hmateu <hmateu.ortola@gmail.com>
2023-06-21 09:10:18 +0200

    feat: Creates the Body and route the views

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './common/Header/Header'
-import { Home } from './pages/Home/Home'
-import { Login } from './pages/Login/Login'
-import { Register } from './pages/Register/Register'
+import { Body } from './pages/Body/Body'
 
 function App() {
 
   return (
     <>
       <Header />
-      <Home />
-      <Login />
-      <Register />
+      <Body />
     </>
   )
 }
