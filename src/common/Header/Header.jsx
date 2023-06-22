6559e5b013817ab537222331b0a916597c467bb0
hmateu <hmateu.ortola@gmail.com>
2023-06-22 08:11:27 +0200

    feat: Improves the layout of navbar elements, both on mobile and PC

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./Header.css";
 import banner from "../../img/banner.png";
 import Navbar from "../Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
 export const Header = () => {
+    const navigate = useNavigate();
     return (
         <div className="headerStyle">
             <div className="left">
-                <img src={banner} />
+                <img src={banner} onClick={()=>{navigate("/")}}/>
             </div>
             <div className="right">
                 <Navbar />
