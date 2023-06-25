e33b076351f0df67490660b6519a9201c4921299
Héctor Mateu <hmateu.ortola@gmail.com>
2023-06-25 11:10:59 +0200

    feat: Calls API to display user names

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Users.css"
+import { bringAllUsers } from "../../utils/apiCalls/usersCalls/usersGetAll";
 export const Users = () => {
+    const [users, setUsers] = useState([]);
+    if(users.length === 0){
+        bringAllUsers()
+        .then(
+            // users => console.log(users.data.data)
+            users => setUsers(users.data.data)
+        )
+        .catch(error=>console.log(error));
+    }
     return (
         <div className="usersStyle">
-            USUARIOS
+            <div className="titleForm">
+                USUARIOS
+            </div>
+            {
+                users.length > 0
+                    ?(
+                        <div className="allUsers">
+                            {
+                                users.map(user => {
+                                    return (
+                                        <div key={user.id}>
+                                            {user.name}
+                                        </div>
+                                    );
+                                })
+                            }
+                        </div>
+                    )
+                    :(<div>CARGANDO ...</div>)
+            }
         </div>
     );
 }
