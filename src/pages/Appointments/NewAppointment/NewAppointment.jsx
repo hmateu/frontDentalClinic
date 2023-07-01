092d1ce6c329658fae0647f0d2ec0d665a1e1f49
hmateu <hmateu.ortola@gmail.com>
2023-07-01 18:05:04 +0200

    feat: Improves the new appointment view

--- src/pages/Appointments/NewAppointment/NewAppointment.jsx
+++ src/pages/Appointments/NewAppointment/NewAppointment.jsx
@@ -4,33 +4,66 @@ import { useNavigate } from "react-router-dom";
 import { FormBtn } from "../../../common/FormBtn/FormBtn";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import SelectService from "../../../common/SelectService/SelectService";
+import useServices from "../../../Hooks/useServices";
 export const NewAppointment = () => {
 
-    const navigate = useNavigate();
+    const [services] = useServices();
+    const [selectedService, setSelectedService] = useState("");
+
+    const selectHandler = (e) => {
+        setSelectedService(e.target.value)
+    }
 
     const [selectedDate, setSelectedDate] = useState(null);
-    const currentDate = new Date();
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
+    const isFutureDate = (date) => {
+        const today = new Date();
+        return date > today;
+    };
+
+    const handlerSubmit = () => {
+        console.log(selectedService)
+        console.log(selectedDate)
+    }
 
     return (
         <div className="newAppointment">
             <div className="viewTitle">NUEVA CITA</div>
             <div className="newAppointmentFormStyle formStyle ">
                 <div className="dataForm">
-                    <SelectService />
+                    <select name="" id="" onChange={selectHandler}>
+                        <option value="">Selecciona un servicio</option>
+                        {
+                            services.map(service => {
+                                return (
+                                    <option key={service.id} value={service.id}>{service.name}</option>
+                                )
+                            })
+                        }
+                    </select>
+                    <div>{selectedService}</div>
                 </div>
 
                 <div className="dataForm">
                     <DatePicker
                         // className={"datePickerStyle"}
                         selected={selectedDate}
-                        minDate={currentDate}
-                        onChange={(date) => setSelectedDate(date)}
+                        onChange={handleDateChange}
+                        minDate={new Date()}
+                        filterDate={isFutureDate}
+                        dateFormat="dd/MM/yyyy"
+                        placeholderText="Selecciona una fecha"
+                        autoComplete="off"
                     />
                 </div>
 
                 <div className={"btnForm"}>
                     <FormBtn
+                    pathClick={handlerSubmit}
                         name={"Confirmar"}
                     />
                 </div>
