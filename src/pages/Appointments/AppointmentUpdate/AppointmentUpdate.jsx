fc4cc55cfd4bf644af1fa757b03c8930e7b7333a
Héctor Mateu <hmateu.ortola@gmail.com>
2023-07-02 19:46:53 +0200

    feat: Adds the functionality to modify appointments by the patient

--- src/pages/Appointments/AppointmentUpdate/AppointmentUpdate.jsx
+++ src/pages/Appointments/AppointmentUpdate/AppointmentUpdate.jsx
@@ -1,15 +1,58 @@
 import React, { useEffect, useState } from "react";
 import "./AppointmentUpdate.css"
-import { useParams } from "react-router-dom";
-import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 import { bringOneAppointment } from "../../../utils/apiCalls/appointmentsCalls/appointmentGetOne";
 import { useSelector } from "react-redux";
 import { userData } from "../../Users/userSlice";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FormBtn } from "../../../common/FormBtn/FormBtn";
+import useServices from "../../../Hooks/useServices";
+import { updateAppointmentByPatient } from "../../../utils/apiCalls/appointmentsCalls/appointmentUpdate";
+import { inputHandler } from "../../../utils/useful";
 export const AppointmentUpdate = () => {
+    const navigate = useNavigate();
     let { id } = useParams();
     const dataRedux = useSelector(userData);
     const token = dataRedux?.credentials?.token;
     const [appointment, setAppointment] = useState({});
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [services] = useServices();
+    const [selectedService, setSelectedService] = useState("");
+    const [data, setData] = useState({});
+
+    const selectHandler = (e) => {
+        setSelectedService(e.target.value)
+    }
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
+    // const [validation, setValidation] = useState(false);
+
+    useEffect(() => {
+        setSelectedService(appointment.service)
+        setSelectedDate(appointment.date)
+  
+    }, []);
+
+    const handlerSubmit = () => {
+        const data = { service: selectedService, date: selectedDate };
+        // console.log(data)
+        updateAppointmentByPatient(token, data, id)
+            .then(
+                setTimeout(() => {
+                    navigate("/appointments");
+                }, 500)
+            )
+    }
+
+    const isFutureDate = (date) => {
+        const today = new Date();
+        return date > today;
+    };
+
     useEffect(() => {
         bringOneAppointment(token, id)
             .then(res => { setAppointment(res[0]) })
@@ -17,17 +60,65 @@ export const AppointmentUpdate = () => {
     }, []);
 
     return (
-        <>
+        <div className="appointmentUpdateStyle">
             <div className="viewTitle">
                 MODIFICAR CITA
             </div>
-            <div className="appointmentUpdateStyle">
-                <div>Actualizar cita número {appointment.id}</div>
-                <div>Fecha: {appointment.date}</div>
-                <div>Precio: {appointment.price}</div>
-                <div>Descripción:{appointment.assessment}</div>
-                <div>Servicio: {appointment.service}</div>
+            <div className="formStyleEditing">
+                <div className="appointmentCard updateAppointment">
+
+                    <div className="dataForm">
+                        <div className="textForm">
+                            Fecha:
+                        </div>
+                        <DatePicker
+                            // className={"datePickerStyle"}
+                            selected={selectedDate}
+                            onChange={handleDateChange}
+                            minDate={new Date()}
+                            filterDate={isFutureDate}
+                            dateFormat="dd/MM/yyyy"
+                            placeholderText={appointment.date}
+                            autoComplete="off"
+                        />
+                    </div>
+                    <div className="dataForm">
+                        <div className="textForm">
+                            Precio:
+                        </div>
+                        {`${appointment.price}€`}
+                    </div>
+                    <div className="dataForm">
+                        <div className="textForm">
+                            Descripción:
+                        </div>
+                        {`${appointment.assessment}`}
+                    </div>
+                    <div className="dataForm">
+                        <div className="textForm">
+                            Servicio:
+                        </div>
+                        {/* <select name="service" id="" onChange={(e)=>inputHandler(e, setData)}> */}
+                        <select name="service" id="" onChange={selectHandler}>
+                            <option value="">{appointment.service}</option>
+                            {
+                                services.map(service => {
+                                    return (
+                                        <option key={service.id} value={service.id}>{service.name}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </div>
+                    {/* <div className={validation ? "btnForm" : "btnForm disabled"}> */}
+                    <FormBtn
+                        pathClick={() => { handlerSubmit(data, token) }}
+                        name={"Confirmar"}
+                    />
+                    {/* </div> */}
+                </div>
+
             </div>
-        </>
+        </div>
     )
 }
---
+++ src/utils/apiCalls/appointmentsCalls/appointmentUpdate.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+export const updateAppointmentByPatient = async (data,token,id) => {
+  try {
+
+    let res = await axios.put(`http://127.0.0.1:5000/appointments/${id}`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log("LLAMADA UPDATEAPPOINTMENT",res.data)
+    return res.data;
+
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+}
